6fa0a1ec3419c07bbcb1ef7d8f65345eb51f806e
Sanja Budic <sanjabudic2907@gmail.com>
2020-09-10 09:32:32 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -37,8 +37,8 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 25),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
