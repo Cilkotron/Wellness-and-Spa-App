7c6321d0e967ad3520ac94158c5d20d198c32edd
Sanja Budic <sanjabudic2907@gmail.com>
2020-09-11 11:37:57 +0200

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
         <title>Spa App</title>
+       <link rel="shortcut icon" href="{{ asset('frontend/images/loading.gif') }}" type="image/x-icon">
         <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.min.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/owl.carousel.css') }}">
