880cd89f0e64d55be62a5b1d62799a0e3129a5b6
Sanja Budic <sanjabudic2907@gmail.com>
2020-10-01 10:43:52 +0200

    Update

--- resources/views/admin/reservation/calendar.blade.php
+++ resources/views/admin/reservation/calendar.blade.php
@@ -106,21 +106,21 @@
         return [year,month,day].join('-')+' '+[hour,minutes,seconds].join(':');
       };
       toastr.options = {
-        "closeButton" => true,
-        "debug" => false,
-        "newestOnTop" => false,
-        "progressBar" => true,
-        "positionClass" => "toast-top-right",
-        "preventDuplicates" => false,
-        "onclick" => null,
-        "showDuration" => "300",
-        "hideDuration" => "1000",
-        "timeOut" => "5000",
-        "extendedTimeOut" => "1000",
-        "showEasing" => "swing",
-        "hideEasing" => "linear",
-        "showMethod" => "fadeIn",
-        "hideMethod" => "fadeOut"
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
 
 
         };
