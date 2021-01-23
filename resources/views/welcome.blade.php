3777a66ca0c69299d084c0b6c7e91351078829d8
Sanja Budic <sanjabudic2907@gmail.com>
2021-01-23 21:09:08 +0100

    Force https

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,6 @@
         <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('frontend/images/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('frontend/images/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('frontend/images/favicon-16x16.png') }}">
-        <link rel="manifest" href="/site.webmanifest">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
         <link rel="stylesheet" href="{{ secure_asset('frontend/css/bootstrap.min.css') }}">
